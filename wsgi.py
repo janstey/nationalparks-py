d973469cd033032fcb4f0ca4893936147d180c70
Jonathan Anstey <janstey@gmail.com>
2017-07-27 13:31:44 -0230

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -32,7 +32,7 @@ api.add_resource(HealthCheck, '/ws/healthz/')
 class Info(Resource):
     description = {
         'id': 'nationalparks-py',
-        'displayName': 'A Fancy National Parks App',
+        'displayName': 'OpenShift National Parks',
         'type': 'cluster',
         'center': {'latitude': '47.039304', 'longitude': '14.505178'},
         'zoom': 4
